709fa15fbebcdfafe892acec3390704eda5e1545
ANIKET  WANI <101266471+ABW1729@users.noreply.github.com>
2024-05-27 11:29:28 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,12 @@
 "use client"
 import Login from '../app/sign-in/SignIn'
-import image from '../../public/stock.webp'
 import Head from 'next/head';
-
+import './global1.css'
 
 export default function Home() {
   return (
     <>
-    <Head>
-        <link rel="stylesheet" href="./global1.css" />
-      </Head>
+ 
      <Login></Login>
     </>
   );
